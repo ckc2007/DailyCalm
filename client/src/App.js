1e923d82bcfb50c3c8db082fd7a97d0b0ef2de43
ckc2007 <ckc2007@gmail.com>
2023-07-27 21:11:28 -0400

    added new App with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,46 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// import SearchBooks from "./pages/SearchBooks";
-// import SavedBooks from "./pages/SavedBooks";
-// import Navbar from "./components/Navbar";
+import SearchCards from "./pages/SearchCards";
+import SavedCards from "./pages/SavedCards";
+import Navbar from "./components/Navbar";
 
 import logo from './logo.svg';
 import './App.css';
 
+// new:
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<SearchCards />} />
+        <Route path="/saved" element={<SavedCards />} />
+        <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} />
+      </Routes>
+    </>
   );
 }
+
+// old:
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;

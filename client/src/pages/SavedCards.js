a7f0012771ea8b37b62e55834848dd2657e9ab54
ckc2007 <ckc2007@gmail.com>
2023-07-27 21:21:14 -0400

    added SavedCards

--- client/src/pages/SavedCards.js
+++ client/src/pages/SavedCards.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
-import { getMe, deleteBook } from "../utils/API";
+import { getMe, deleteCard } from "../utils/API";
 import { authService } from "../utils/auth";
-import { removeBookId } from "../utils/localStorage";
+import { removeCardId } from "../utils/localStorage";
 
-const SavedBooks = () => {
+const SavedCards = () => {
   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
@@ -36,8 +36,8 @@ const SavedBooks = () => {
     getUserData();
   }, [userDataLength]);
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
+  // create function that accepts the card's mongo _id value as param and deletes the card from the database
+  const handleDeleteCard = async (cardId) => {
     const token = authService.loggedIn() ? authService.getToken() : null;
 
     if (!token) {
@@ -45,21 +45,21 @@ const SavedBooks = () => {
     }
 
     try {
-      await deleteBook(bookId, token);
+      await deleteCard(cardId, token);
   
-      // If the deleteBook function does not throw an error,
-      // it means the book was deleted successfully.
-      // So, update the user data and remove the book's id from localStorage.
+      // If the deleteCard function does not throw an error,
+      // it means the card was deleted successfully.
+      // So, update the user data and remove the card's id from localStorage.
       const updatedUser = { ...userData };
-      updatedUser.savedBooks = updatedUser.savedBooks.filter(
-        (book) => book.bookId !== bookId
+      updatedUser.savedCards = updatedUser.savedCards.filter(
+        (card) => card.cardId !== cardId
       );
       setUserData(updatedUser);
-      removeBookId(bookId);
+      removeCardId(cardId);
     } catch (err) {
       console.error(err);
       // Handle the error message if needed
-      console.log("Error deleting the book:", err.message);
+      console.log("Error deleting the card:", err.message);
       // ...
     }
   };
@@ -73,38 +73,38 @@ const SavedBooks = () => {
     <>
       <div fluid className="text-light bg-dark p-5">
         <Container>
-          <h1>Viewing saved books!</h1>
+          <h1>Viewing saved cards!</h1>
         </Container>
       </div>
       <Container>
         <h2 className="pt-5">
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? "book" : "books"
+          {userData.savedCards.length
+            ? `Viewing ${userData.savedCards.length} saved ${
+                userData.savedCards.length === 1 ? "card" : "cards"
               }:`
-            : "You have no saved books!"}
+            : "You have no saved cards!"}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedCards.map((card) => {
             return (
-              <Col key={book.bookId} md="4">
-                <Card key={book.bookId} border="dark">
-                  {book.image ? (
+              <Col key={card.cardId} md="4">
+                <Card key={card.cardId} border="dark">
+                  {card.image ? (
                     <Card.Img
-                      src={book.image}
-                      alt={`The cover for ${book.title}`}
+                      src={card.image}
+                      alt={`The cover for ${card.title}`}
                       variant="top"
                     />
                   ) : null}
                   <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className="small">Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
+                    <Card.Title>{card.title}</Card.Title>
+                    <p className="small">Authors: {card.authors}</p>
+                    <Card.Text>{card.description}</Card.Text>
                     <Button
                       className="btn-block btn-danger"
-                      onClick={() => handleDeleteBook(book.bookId)}
+                      onClick={() => handleDeleteCard(card.cardId)}
                     >
-                      Delete this Book!
+                      Delete this Card!
                     </Button>
                   </Card.Body>
                 </Card>
@@ -117,4 +117,4 @@ const SavedBooks = () => {
   );
 };
 
-export default SavedBooks;
+export default SavedCards;

e9425c0dd72a56f4d11e06f7b1562160b6457b7d
ckc2007 <ckc2007@gmail.com>
2023-07-30 16:56:15 -0400

    fixing Card model

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router } from "react-router-dom";
+// adding bootstrap back for now - chorney
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import ApolloClient from "./utils/ApolloClient";
@@ -33,4 +35,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();

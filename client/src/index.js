2a9504503b0dbbb26c460e77e194dbdb036c8d7e
ckc2007 <ckc2007@gmail.com>
2023-08-02 20:18:25 -0400

    cleaning up index.js in client

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router } from "react-router-dom";
-// adding bootstrap back for now - chorney
 // changed to bulma - Sean
 import 'bulma/css/bulma.min.css';
 import "./index.css";
@@ -10,7 +9,7 @@ import App from "./App";
 import ApolloClient from "./utils/ApolloClient";
 // import reportWebVitals from "./reportWebVitals";
 
-console.log(localStorage.getItem("token")); // Log the token
+// console.log(localStorage.getItem("token")); // Log the token
 
 // new:
 ReactDOM.render(
@@ -23,14 +22,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// old:
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // test:
 // If you want to start measuring performance in your app, pass a function

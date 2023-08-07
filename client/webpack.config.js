4390fae46ab14fb5f7abdff9b09bc5b74c0b1be0
ckc2007 <ckc2007@gmail.com>
2023-08-07 00:40:29 -0400

    fixing service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,51 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     // Entry point for files
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      main: "./src/index.js",
     },
     // Output for our bundles
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
+      // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Contact Cards'
+        template: "./public/index.html",
+        title: "DailyCalm App",
       }),
-     
+      new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
       // Injects our custom service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
 
       // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        name: "DailyCalm React App", // Update the name
+        short_name: "DailyCalm", // Update the short_name
+        description: "Your description here", // Update the description
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        start_url: ".", // Update the start_url
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("public/logo192.png"), // Update the icon source
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
@@ -56,21 +58,27 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           // We use babel-loader in order to use ES6.
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
       ],
+    },
+    devServer: {
+      historyApiFallback: true, // Enable HTML5 history routing for React Router
     },
   };
 };
